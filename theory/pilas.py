46adfcab67244765e0962c2788dc1faea16426bf
Loncho-richard <carlosalbertorichardson@gmail.com>
2022-03-25 13:07:11 -0300

    update

---
+++ theory/pilas.py
@@ -0,0 +1,16 @@
+pila=[]
+print("Entrada")
+pila.append("T")
+print("T")
+pila.append("E")
+print("E")
+pila.append("C")
+print("C")
+
+print("Salida")
+n=pila.pop()
+print(n)
+n=pila.pop()
+print(n)
+n=pila.pop()
+print(n)
