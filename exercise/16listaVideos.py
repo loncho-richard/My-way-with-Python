727c1cb4492106f289f6aac68903c341713e1aca
Loncho-richard <carlosalbertorichardson@gmail.com>
2022-03-28 11:02:56 -0300

    update

---
+++ exercise/16listaVideos.py
@@ -0,0 +1,14 @@
+'''
+Crearemos una lista vacía.
+Crearemos un diccionario con 3 llaves y valores: nombre, url y duracion.
+Agregaremos cada diccionarios nuestra lista.
+Mostraremos el resultado.
+'''
+
+videos=[]
+v={"nombre":"01.- Tutorial python","url":"youtube.com/lkdslkdjs","duracion":500}
+videos.append(v)
+v={"nombre":"01.- Tutorial python","url":"youtube.com/lkds","duracion":300}
+videos.append(v)
+v={"nombre":"01.- Tutorial python","url":"youtube.com/lkdslk","duracion":700}
+print(videos)
