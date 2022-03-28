46b71ce603bdd8f01d632d287005861e1dbd379e
Loncho-richard <carlosalbertorichardson@gmail.com>
2022-03-28 14:14:53 -0300

    update

---
+++ theory/DoWhile.py
@@ -0,0 +1,14 @@
+#con dowhile
+'''
+while True:
+    letra=input("Presiona laletra 'a':")
+    if letra!='a':
+        break
+print("Te dije que presionaras letra 'a'")
+'''
+
+#Con while
+letra=input("Humano digital la letra 'a':")
+while letra=='a':
+    letra=input("Humano digital la letra 'a':")
+print("Humano tonto te dije que precionaras la letra 'a'")
