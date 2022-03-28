cd368c5fc7466a47f61c7af5600ab32de30b387d
Loncho-richard <carlosalbertorichardson@gmail.com>
2022-03-28 11:42:35 -0300

    update

---
+++ theory/ciclosWhile.py
@@ -0,0 +1,12 @@
+contador=4
+contador2=4
+
+while contador>0:
+    print(f"contador={contador}")
+    contador-=2
+print("Fin del ciclo")
+
+while contador<5:
+    print(f"contador={contador}")
+    contador+=1
+print("Fin del ciclo")
