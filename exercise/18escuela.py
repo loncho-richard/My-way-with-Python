4e9d1dd097b79ad496cbf8abcd78ca111c6abacb
Loncho-richard <carlosalbertorichardson@gmail.com>
2022-03-29 00:31:49 -0300

    update

---
+++ exercise/18escuela.py
@@ -0,0 +1,49 @@
+'''
+En este ejercicio vamos a acceder a una lista de alumnos que tengan:
+nombre
+edad
+calificación
+Además un diccionario que tenga alumnos y maestros donde:
+Los alumnos tendrán:
+nombre
+edad
+calificación
+Y los maestros tendrán:
+nombre
+grado
+'''
+
+'''
+alumnos=[
+    {"nombre":"Santino","edad":17,"calificacion":8},
+    {"nombre":"Valentin","edad":17,"calificacion":9},
+    {"nombre":"Alejo","edad":17,"calificacion":7},
+    {"nombre":"Mariano","edad":16,"calificacion":10},
+]
+'''
+
+#for alumno in alumnos:
+ #   print(f"Nombre:{alumno['nombre']} Edad:{alumno['edad']} Calificacion:{alumno['calificacion']}")
+
+escuela={
+    "alumnos":[
+        {"nombre":"Santino","edad":17,"calificacion":8},
+        {"nombre":"Valentin","edad":17,"calificacion":9},
+        {"nombre":"Alejo","edad":17,"calificacion":7},
+        {"nombre":"Mariano","edad":16,"calificacion":10},
+    ],
+    "maestros":[
+        {"nombre":"Carlos","grado":"Licenciatura"},
+        {"nombre":"Maria Eugenia","grado":"Mestria"},
+        {"nombre":"Pichi","grado":"Doctorado"},
+    ]
+}
+
+print("ALUMNOS")
+for alumno in escuela["alumnos"]:
+    print(f"Nombre: {alumno['nombre']} Edad: {alumno['edad']} Calificaion: {alumno['calificacion']}")
+print("\nMAESTROS")
+for maestro in escuela["maestros"]:
+    print(f"Nombre: {maestro['nombre']} Grado: {maestro['grado']}")
+
+print(escuela)
