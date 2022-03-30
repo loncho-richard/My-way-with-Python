e711c3556d8dab4dfa5288855793c1e3d07a5bf1
Loncho-richard <carlosalbertorichardson@gmail.com>
2022-03-30 00:42:23 -0300

    update

--- exercise/21sumaCuadrados.py
+++ exercise/21sumaCuadrados.py
@@ -3,4 +3,4 @@ for i in range(1,11):
     cuadrado=(i)*(i)
     suma=suma+cuadrado
     print(f"El cuadrado es de {i} = {cuadrado}")
-print(F"La suma de los cuadrados es: {suma}")
+print(f"La suma de los cuadrados es: {suma}")
