a279ce2df88b1d199bcc6b7b032a349f67106672
Loncho-richard <carlosalbertorichardson@gmail.com>
2022-05-10 15:01:15 -0300

    update veterinaria

--- exercise/veterinaria.py
+++ exercise/veterinaria.py
@@ -1,21 +1,37 @@
-database=[]
+databasePerro=[]
+databaseGato=[]
 class Mascota:
     def __init__(self,name,old,_type):
         self.name=name
         self.old=old
         self._type=_type
     @classmethod
-    def entrada_mascota(self,name):
-        database.append(name)
+    def entrada_mascota(self,name,_type):
+        if _type=="perro":
+            databasePerro.append(name)
+        elif _type=="gato":
+            databaseGato.append(name)
+
     @classmethod
-    def salida_mascota(self,name):
-        database.remove(name)
+    def salida_mascota(self,name,_type):
+        if _type=="perro":
+            databasePerro.remove(name)
+        elif _type=="gato":
+            databaseGato.remove(name)
+
+
 perro1=Mascota("Tino",4,"perro")
 perro2=Mascota("Ciro",3,"perro")
-print(database)
-Mascota.entrada_mascota(perro1.name)
-Mascota.entrada_mascota(perro2.name)
-print(database)
-Mascota.salida_mascota(perro2.name)
-print(database)
-#print(perro.name, perro.old, perro.type)
+perro3=Mascota("Choco",5,"perro")
+gato1=Mascota("Michi",2,"gato")
+
+print(databasePerro,databaseGato)
+Mascota.entrada_mascota(perro1.name,perro1._type)
+Mascota.entrada_mascota(perro2.name,perro2._type)
+Mascota.entrada_mascota(perro3.name,perro3._type)
+Mascota.entrada_mascota(gato1.name,gato1._type)
+print("Perros:",databasePerro,"\nGatos:",databaseGato)
+Mascota.salida_mascota(gato1.name,gato1._type)
+Mascota.salida_mascota(perro2.name,perro2._type)
+print("Perros:",databasePerro,"\nGatos:",databaseGato)
+
