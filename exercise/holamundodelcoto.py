16afd79c18da567048ab93f5256e6776749938c8
Loncho-richard <carlosalbertorichardson@gmail.com>
2022-05-17 22:10:11 -0300

    update

--- exercise/holamundodelcoto.py
+++ exercise/holamundodelcoto.py
@@ -11,8 +11,17 @@ if variable>2:
     print ("sos mogolico")
 elif variable<1:
     print ("sos mogolico")
-'''
+
 
 n1=int(input("Ingresar el valor de numero n1: "))
 n2=int(input("Ingresar el valor de numero n2: "))
 print(f"El resultado de tu suma de {n1} + {n2} es {n1+n2}")
+'''
+
+
+n1=int(input("Ingresa el valor de n1: "))
+n2=int(input("Ingresa el valor de n2: "))
+
+resultado=n1+n2
+
+print(resultado)
