f275bc5f13199ef7477c6f035cddb82e80efdb38
Loncho-richard <carlosalbertorichardson@gmail.com>
2022-05-22 23:34:04 -0300

    update files

--- theory/archivo.py
+++ theory/archivo.py
@@ -1,4 +1,22 @@
 #absoluta="C:\\Users\\carlo\\OneDrive\\workspace\\python\\My-way-with-Python\\theory\\archivo\\hola.txt"
-relativa="archivo\\hola.txt"
+relativa="theory\\archivo\\hola.txt"
 archivo=open(relativa,"r")
-print(archivo)
+#print(archivo.read())
+#print(archivo.readline())
+#
+#for linea in archivo.readlines():
+#    print(linea)
+
+#ESCRITURA
+
+#relativa2="theory\\archivo\\hola2.txt"
+#archivo2=open(relativa2, "w")
+#archivo2.write("C#\nVB.net\nASP.net")
+
+#ESCRITURA - LECTURA
+
+relativa3 = "theory\\archivo\\hola3.txt"
+archivo3 = open(relativa3, "r+")
+archivo3.write("Hola\nComo\nEstas")
+archivo3.seek(0)
+print(archivo3.read())
