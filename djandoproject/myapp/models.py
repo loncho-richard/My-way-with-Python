8b368c5c1cc81bcbf45b930152ec96b32d47ffe4
Carlos Richardson <carlosalbertorichardson@gmail.com>
2022-10-06 12:59:05 -0300

    Update

--- djandoproject/myapp/models.py
+++ djandoproject/myapp/models.py
@@ -1,3 +1,17 @@
+from turtle import title
 from django.db import models
 
 # Create your models here.
+class Projects(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    project = models.ForeignKey(Projects, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title + ' - ' + self.project.name
--- djandoproject/myapp/views.py
+++ djandoproject/myapp/views.py
@@ -1,9 +1,32 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Projects, Task
+from django.shortcuts import render
 
 # Create your views here.
-def hello(request):
-    return HttpResponse("<h1>Hello world</h1>")
+def index(request):
+    title = 'Djando course!!'
+    return render(request, 'index.html', {
+        'title': title
+    })
+
+def hello(request, username):
+    return HttpResponse("<h1>Hello %s</h1>" % username)
 
 def about(request):
-    return HttpResponse("about")
+    username = 'loncho'
+    return render(request, 'about.html', {
+        'username': username
+    })
+def projects(request):
+    #projects = list(Projects.objects.values())
+    projects = Projects.objects.all()
+    return render(request, 'projects.html', {
+        'projects': projects
+    })
+
+def tasks(request):
+    #task = get_object_or_404(Task, title = title)
+    tasks = Task.objects.all()
+    return render(request, 'tasks.html', {
+        'tasks': tasks
+    })
